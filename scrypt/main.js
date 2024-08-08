94f493a42b800273c7ba894a48fc0cc3e65f42dd
cavedium <shports61@gmail.com>
2024-08-08 13:36:31 +0300

    add changing and saving some pictures

--- scrypt/main.js
+++ scrypt/main.js
@@ -1,4 +1,35 @@
 const nightThemeBtn = document.getElementById("night-theme-btn")
+const checkbox = document.getElementById("night-theme-btn")
+
+let hero = document.getElementById("hero-img")
+let arrow = document.getElementById("arrow-scroll")
+let gmail = document.getElementById("gmail")
+let linkedin = document.getElementById("linkedin")
+let github = document.getElementById("github")
+
+if (localStorage.getItem("theme") == "night-theme") {
+	checkbox.checked = true
+}
+
+if (localStorage.getItem("hero") == "true") {
+	hero.setAttribute('src', './img/dark-hero.png')
+}
+
+if (localStorage.getItem("arrow") == "true") {
+	arrow.setAttribute('src', './img/dark-arrow-scroll.svg')
+}
+
+if (localStorage.getItem("gmail") == "true") {
+	gmail.setAttribute('src', './img/dark-gmail-icon.svg')
+}
+
+if (localStorage.getItem("linkedin") == "true") {
+	linkedin.setAttribute('src', './img/dark-linkedin-icon.svg')
+}
+
+if (localStorage.getItem("github") == "true") {
+	github.setAttribute('src', './img/dark-github-icon.svg')
+}
 
 nightThemeBtn.addEventListener("click", function () {
 	document.body.classList.toggle("night-theme")
@@ -10,47 +41,28 @@ nightThemeBtn.addEventListener("click", function () {
 	} else {
 		localStorage.setItem("theme", "night-theme")
 	}
-
-	let image = document.getElementById("hero-img")
-
-	if (image.src.match('./img/hero.png')) {
-		image.src = './img/dark-hero.png'
+	
+	if (theme === "night-theme") {
+		localStorage.setItem("hero", false)
+		localStorage.setItem("arrow", false)
+		localStorage.setItem("gmail", false)
+		localStorage.setItem("linkedin", false)
+		localStorage.setItem("github", false)
+		hero.setAttribute('src', './img/hero.png')
+		arrow.setAttribute('src', './img/arrow-scroll.svg')
+		gmail.setAttribute('src', './img/gmail-icon.svg')
+		linkedin.setAttribute('src', './img/linkedin-icon.svg')
+		github.setAttribute('src', './img/github-icon.svg')
 	} else {
-		image.src = './img/hero.png'
+		localStorage.setItem("hero", true)
+		localStorage.setItem("arrow", true)
+		localStorage.setItem("gmail", true)
+		localStorage.setItem("linkedin", true)
+		localStorage.setItem("github", true)
+		hero.setAttribute('src', './img/dark-hero.png')
+		arrow.setAttribute('src', './img/dark-arrow-scroll.svg')
+		gmail.setAttribute('src', './img/dark-gmail-icon.svg')
+		linkedin.setAttribute('src', './img/dark-linkedin-icon.svg')
+		github.setAttribute('src', './img/dark-github-icon.svg')
 	}
-
-	let arrow = document.getElementById("arrow-scroll")
-
-	if (arrow.src.match('./img/arrow-scroll.svg')) {
-		arrow.src = './img/dark-arrow-scroll.svg'
-	} else {
-		arrow.src = './img/arrow-scroll.svg'
-	}
-
-	let gmail = document.getElementById("gmail")
-
-	if (gmail.src.match('./img/gmail-icon.svg')) {
-		gmail.src = './img/dark-gmail-icon.svg'
-	} else {
-		gmail.src = './img/gmail-icon.svg'
-	}
-
-	let linkedin = document.getElementById("linkedin")
-
-	if (linkedin.src.match('./img/linkedin-icon.svg')) {
-		linkedin.src = './img/dark-linkedin-icon.svg'
-	} else {
-		linkedin.src = './img/linkedin-icon.svg'
-	}
-
-	let github = document.getElementById("github")
-
-	if (github.src.match('./img/github-icon.svg')) {
-		github.src = './img/dark-github-icon.svg'
-	} else {
-		github.src = './img/github-icon.svg'
-	}
-
 })
-
-
